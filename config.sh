432309d9a495e63a41308a5d84b8a911983c7f5b
Vedraj Gawas <gawasvedraj@gmail.com>
2025-08-24 11:10:08 +0530

    test

--- config.sh
+++ config.sh
@@ -13,20 +13,26 @@ touch $KERNEL_DIR/.scmversion
 
 msg "KernelSU"
 if [[ $KSU_ENABLED == "true" ]]; then
-    cd $KERNEL_DIR && curl https://raw.githubusercontent.com/$KERNELSU_REPO/refs/heads/master/kernel/setup.sh | bash -s $KERNELSU_BRANCH
+    cd $KERNEL_DIR && curl https://raw.githubusercontent.com/$KERNELSU_REPO/refs/heads/main/kernel/setup.sh | bash -s $KERNELSU_BRANCH
     msg "Importing KernelSU..."
 
-    cd KernelSU && curl https://raw.githubusercontent.com/gawasvedraj/KernelOwO/refs/heads/master/patches/0001-Remove-unnecessary-manager-signatures.patch | patch -p1 -F3 && msg "Signatures patched successfully"
     cd $KERNEL_DIR
-
-    git clone https://gitlab.com/simonpunk/susfs4ksu -b kernel-5.4 susfs4ksu
-    cp susfs4ksu/kernel_patches/fs/* fs/
-    cp susfs4ksu/kernel_patches/include/linux/* include/linux/
-    patch -p1 -F 3 < susfs4ksu/kernel_patches/50_add_susfs_in_kernel-5.4.patch
-    msg "Importing SuSFS into 5.4 kernel..."
 
     echo "CONFIG_KSU=y" >> $DEVICE_DEFCONFIG_FILE
     echo "CONFIG_KSU_SUSFS=y" >> $DEVICE_DEFCONFIG_FILE
+    echo "CONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT=y" >> $DEVICE_DEFCONFIG_FILE
+    echo "CONFIG_KSU_SUSFS_SUS_PATH=y" >> $DEVICE_DEFCONFIG_FILE
+    echo "CONFIG_KSU_SUSFS_SUS_MOUNT=y" >> $DEVICE_DEFCONFIG_FILE
+    echo "CONFIG_KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT=y" >> $DEVICE_DEFCONFIG_FILE
+    echo "CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT=y" >> $DEVICE_DEFCONFIG_FILE
+    echo "CONFIG_KSU_SUSFS_TRY_UMOUNT=y" >> $DEVICE_DEFCONFIG_FILE
+    echo "CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT=y" >> $DEVICE_DEFCONFIG_FILE
+    echo "CONFIG_KSU_SUSFS_SPOOF_UNAME=y" >> $DEVICE_DEFCONFIG_FILE
+    echo "CONFIG_KSU_SUSFS_ENABLE_LOG=y" >> $DEVICE_DEFCONFIG_FILE
+    echo "CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS=y" >> $DEVICE_DEFCONFIG_FILE
+    echo "CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG=y" >> $DEVICE_DEFCONFIG_FILE
+    echo "CONFIG_KSU_SUSFS_OPEN_REDIRECT=y" >> $DEVICE_DEFCONFIG_FILE
+    echo "CONFIG_KSU_SUSFS_SUS_SU=n" >> $DEVICE_DEFCONFIG_FILE
     echo "CONFIG_KPROBES=n" >> $DEVICE_DEFCONFIG_FILE # it will conflict with KSU hooks if it's on
 
     KSU_GIT_VERSION=$(cd KernelSU && git rev-list --count HEAD)

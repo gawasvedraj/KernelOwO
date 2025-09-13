20c47469d8e686214b4e6dc22583a09569de768b
Vedraj Gawas <gawasvedraj@gmail.com>
2025-09-14 00:58:31 +0530

    Python 2 is EOL

--- env.sh
+++ env.sh
@@ -8,8 +8,3 @@ apt autoremove
 
 # Environment Setup
 apt-get update && apt-get upgrade -y
-
-# Install python2 if Ubuntu version is lower than 24.04
-if [[ $UBUNTU_VERSION != "24.04" ]]; then
-    apt install python2
-fi
